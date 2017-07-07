546df2542779c30915aceb3826dfbfedb7874fcb
Joe <joevzq@gmail.com>
2017-07-07 17:22:28 -0500

    Add rng(), cud(), c_unif() and d_unif. Exchange every instance of rand() for an rng() implementation

--- SimpleGeneticAlgorithm.c
+++ SimpleGeneticAlgorithm.c
@@ -6,8 +6,7 @@
 #include <unistd.h>
 #include "../libraries/gnuplot_i/src/gnuplot_i.h"
 
-/*  TODO: Random function
-    TODO: Ints to char array
+/*  TODO: Ints to char array
     TODO: Calculate bits needed
     TODO: Implement Error Tolerance (EPSILON)
     TODO: Show plot for standard deviation, avg fitness, max fitness for each GENERATIONS
@@ -33,6 +32,7 @@ struct chromosome{
     double  expected_p;     /* Expected population                      */
 };
 
+/* Functions */
 void                crossover           (uint8_t x1, uint8_t x2, struct chromosome *child1, struct chromosome *child2);
 void                show_bits           (uint8_t x);
 int64_t             f                   (uint8_t x);
@@ -43,12 +43,36 @@ void                get_probabilites    (struct chromosome p[POPULATION_SIZE], i
 struct chromosome   *select_parent      (struct chromosome p[POPULATION_SIZE], int64_t *sof);
 void                procreate           (struct chromosome p[POPULATION_SIZE], struct chromosome pnext[POPULATION_SIZE], int64_t *sof);
 void                run                 (struct chromosome p[POPULATION_SIZE], uint16_t iters);
+double              rng                 ();
+double              cud                 (double x, double a, double b);
+double              c_unif              (double x, double a, double b);
+uint16_t            d_unif              (double x, double a, double b);
+
 
 int main() {
+
     struct chromosome population[POPULATION_SIZE];
     uint16_t iterations;
+    /* |||||||||||||||||||||||||||||||||||||||||||||||||| */
+    /* |||||||||||||||||| TESTING AREA |||||||||||||||||| */
+    /* |||||||||||||||||||||||||||||||||||||||||||||||||| */
+    //printf("rng: %f\n", rng());
+    //uint8_t cutpoint = d_unif(rng(), 1, sizeof(uint8_t)*8);
+    //printf("cutpoint: %d\n", cutpoint);
+    //uint8_t mask1 = pow(2, cutpoint) - 1;
+    //printf("mask1: ");
+    //show_bits(mask1);
 
-    RNG();
+    //printf("sizeof(uint8_t)*8: %lu\n", sizeof(uint8_t)*8);
+    //uint8_t maxvalue = pow(2, sizeof(uint8_t)*8)-1;
+    //printf("maxvalue: %d\n", maxvalue);
+
+    //printf("discrete rng (1-5)[1,6]:\t%d\n", d_unif(rng(), 1, 6));
+    //printf("continuous rng (1-5)[1,6]:\t%f\n", c_unif(rng(), 1, 6));
+    /* |||||||||||||||||||||||||||||||||||||||||||||||||| */
+    /* |||||||||||||||||||||||||||||||||||||||||||||||||| */
+    /* |||||||||||||||||||||||||||||||||||||||||||||||||| */
+
     /*  UI */
     printf("\nWelcome to this Simple Genetic Algorithm Software written by Joe Vázquez-Mellado\n");
     printf("\nPlease enter number of iterations: ");
@@ -110,10 +134,11 @@ void run(struct chromosome p[POPULATION_SIZE], uint16_t iters) {
 }
 
 /*  Cryptographically Secure Pseudornadom Number Generator
-    Read bytes from /dev/random device file. Each by from this files is
+    Read bytes from /dev/random device file. Each byte from the file is
     a cryptographically random value from 0-255. This function concatenates
-    thos bytes to generate a random number of an arbitrary size. */
-uint32_t rng() {
+    those bytes to generate a random number of an arbitrary size, only to then
+    get a point value out of it. */
+double rng() {
     /* Establish connection with /dev/random */
     FILE *fp = fopen("/dev/random", "r");
 
@@ -122,15 +147,36 @@ uint32_t rng() {
         perror("RNG");
         exit(-1);
     }
+    uint16_t    true_value = 0;
+    double      point_value;
 
-    uint32_t value = 0;
-
-    for (uint8_t i = 0; i < sizeof(value); i++) {
-        value <<= 8;
-        value |= fgetc(fp);
+    for (uint8_t i = 0; i < sizeof(true_value); i++) {
+        true_value <<= 8;
+        true_value |= fgetc(fp);
     }
+    /* Get a point value from 0 to 1 */
+    point_value = (double)true_value / (pow(2,(sizeof(true_value)*8)) - 1);
+    /* Close connection with /dev/random */
     fclose(fp);
-    printf("\nRNG:\t%d\n", value);
+    //printf("\nRNG true_value: %u\tpoint_value: %f\n", true_value, point_value);
+    return point_value;
+}
+
+/* Discrete Uniform Value.
+    With a point value of x when 0 ≤ x ≤ 1, get an uniformly
+    equivalent int value y as a ≤ y < b*/
+uint16_t d_unif(double x, double a, double b) {
+    return (x == 1) ? (uint16_t)b-1 : (uint16_t)floor(x*(b-a) + a );
+}
+/*  Continuous Uniform Value.
+    With a point value of x when 0 ≤ x ≤ 1, get an uniformly
+    equivalent point value y as a ≤ y < b */
+double c_unif(double x, double a, double b) {
+    return (x == 1) ? b-0.000001 : x*(b-a) + a;
+}
+/*  Continuous Uniform Distribution */
+double cud(double x, double a, double b) {
+    return (x-a) / (b-a) * (b-a);
 }
 
 
@@ -141,28 +187,31 @@ void procreate(struct chromosome p[POPULATION_SIZE], struct chromosome pnext[POP
     for (uint16_t i = 0; i < POPULATION_SIZE; i += 2) {
         parent1 = select_parent(p, sof);
         parent2 = select_parent(p, sof);
+
         //printf("\nparent1: x: %d\ttrue fitness: %lld\tfitness: %llu\tprobability: %f\tcdf: %f\t - %p\n", parent1->x, parent1->true_fitness,parent1->fitness, parent1->probability, parent1->cdf, &parent1);
         //printf("parent2: x: %d\ttrue fitness: %lld\tfitness: %llu\tprobability: %f\tcdf: %f\t - %p\n", parent2->x, parent2->true_fitness, parent2->fitness, parent2->probability, parent2->cdf, &parent2);
         //show_bits(parent1->x);
         //show_bits(parent2->x);
 
         /* Do crossover if CROSSOVER_RATE is met */
-        if ( (double)(rand() % 1000) / 1000 <= CROSSOVER_RATE )
+        if (rng() <= CROSSOVER_RATE)
             crossover(parent1->x, parent2->x, &child1, &child2);
+
         //printf("crossover after:\n");
         //show_bits(child1.x);
         //show_bits(child2.x);
         //printf("mutation after:\n");
 
         /* Do mutation if MUTATION_RATE is met */
-        if ( (double)(rand() % 1000) / 1000 <= MUTATION_RATE )
+        if (rng() <= MUTATION_RATE)
             mutate(&child1.x);
-        if ( (double)(rand() % 1000) / 1000 <= MUTATION_RATE )
+        if (rng() <= MUTATION_RATE )
             mutate(&child2.x);
 
         /* Assign children to new generation */
         pnext[i]    = child1;
         pnext[i+1]  = child2;
+
         //printf("\nchilds bits:\n");
         //printf("child1: x: %d - %p\n", child1.x, &child1);
         //printf("child2: x: %d - %p\n", child2.x, &child2);
@@ -176,12 +225,15 @@ void procreate(struct chromosome p[POPULATION_SIZE], struct chromosome pnext[POP
 struct chromosome *select_parent(struct chromosome p[POPULATION_SIZE], int64_t *sof) {
     double roulette_shot;
     uint16_t i      = 0;
-    roulette_shot   = (double)(rand() % *sof) / (double)*sof;
+    roulette_shot   = rng();
+
     //printf("\nroulette_shot: %f\n", roulette_shot);
+
     while (roulette_shot > p[i].cdf)
         i++;
 
     //printf("p[%d]: x: %d\ttrue_fitness: %lld\tfitness: %llu\tprobability: %f\tcdf: %f\t - %p\n", i, p[i].x, p[i].true_fitness, p[i].fitness, p[i].probability, p[i].cdf, &p[i]);
+
     return &p[i];
 }
 
@@ -193,6 +245,7 @@ void get_probabilites(struct chromosome p[POPULATION_SIZE], int64_t sof) {
         p[i].probability    = (double)p[i].fitness / (double)sof;
         sum_pr              += p[i].probability;
         p[i].cdf            = sum_pr;
+
         printf("p[%d]: x: %d\ttrue_fitness: %lld\tfitness: %llu\tprobability: %f\tcdf: %f\t %p\n", i, p[i].x, p[i].true_fitness, p[i].fitness, p[i].probability, p[i].cdf, &p[i]);
     }
 }
@@ -209,6 +262,7 @@ int64_t get_fitness(struct chromosome p[POPULATION_SIZE]) {
         p[i].true_fitness   = p[i].fitness;
         if (p[i].fitness < min)
             min = p[i].fitness;
+
         //printf("p[%d] x: %d\tfitness: %lld\tmin: %lld\n", i, p[i].x, p[i].fitness, min);
     }
 
@@ -216,9 +270,12 @@ int64_t get_fitness(struct chromosome p[POPULATION_SIZE]) {
     for (uint16_t i = 0; i < POPULATION_SIZE; i++) {
         p[i].fitness    += labs(min);
         sum             += p[i].fitness;
+
         //printf("p[%d] x: %d\tfitness: %lld\tsum: %lld\n", i, p[i].x, p[i].fitness, sum);
     }
+
     //printf("\n\n SUM OF FITNESS: %lld\n", sum);
+
     return sum;
 }
 
@@ -226,7 +283,7 @@ int64_t get_fitness(struct chromosome p[POPULATION_SIZE]) {
 /*  Mutation Operator.
     Create a random gene of a given chromosome */
 void mutate(uint8_t *x) {
-    uint8_t bit_to_mutate   = rand() % (sizeof(uint8_t)*8 -1);
+    uint8_t bit_to_mutate   = d_unif(rng(), 0, sizeof(uint8_t)*8);
     uint8_t  mask           = 1 << bit_to_mutate;
     *x                      ^= mask;
 }
@@ -235,9 +292,12 @@ void mutate(uint8_t *x) {
 /* Create starting population */
 void first_gen(struct chromosome p[POPULATION_SIZE]) {
     /* Set maxvalue of each chromosome based off the chromosome's size */
-    uint8_t maxvalue = pow(2, sizeof(uint8_t)*8) - 1;
+    uint8_t maxvalue = pow(2, sizeof(uint8_t)*8 - 1);
+    //printf("maxvalue: %d\n", maxvalue);
     for (int i = 0; i < POPULATION_SIZE; i++){
-        p[i].x = rand() % maxvalue;
+        uint8_t rnd = d_unif(rng(), 0, maxvalue+1);
+    //    printf("rnd: %d", rnd);
+        p[i].x = rnd;
         show_bits(p[i].x);
     }
 }
@@ -248,10 +308,13 @@ void first_gen(struct chromosome p[POPULATION_SIZE]) {
 void crossover(uint8_t x1, uint8_t x2, struct chromosome *child1, struct chromosome *child2) {
     /*  Get random cutpoint's locus
         cutpoint >= 0 && cutpoint < chromosome's size */
-    uint8_t cutpoint = rand() % ( sizeof(uint8_t)*8 - 1 );
+    uint8_t cutpoint = d_unif(rng(), 1, sizeof(uint8_t)*8);
+    //printf("cutpoint: %d", cutpoint);
     /*  Get mask with all 0's left to cutpoint's locus and all 1's to its right
         Used for & bit operation */
     uint8_t mask1 = pow(2, cutpoint) - 1;
+    //printf("mask1: ");
+    show_bits(mask1);
     /*  Get mask with all 1's left to cutpoint's locus and all 0s to its right
         Used for | bit operation */
     uint8_t mask2 = pow(2, sizeof(uint8_t)*8) - 1 - mask1;
@@ -271,7 +334,7 @@ int64_t f(uint8_t x) {
 
 /*  Print bit array of a given number */
 void show_bits(uint8_t x) {
-        for (int i = (sizeof(uint8_t)*8) - 1; i >= 0; i--)
-            (x & (1u << i)) ? putchar('1') : putchar('0');
-    	printf("\n");
+    for (int i = (sizeof(uint8_t)*8) - 1; i >= 0; i--)
+        (x & (1u << i)) ? putchar('1') : putchar('0');
+	printf("\n");
 }
